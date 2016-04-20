778ab554df6a1b01b680702090be4ad042338638
Yuriy <yberdnikov@me.com>
2016-04-20 11:39:48 +0300

    Initial commit.

---
+++ weather/weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weather
+//
+//  Created by Yuriy Berdnikov on 4/20/16.
+//  Copyright © 2016 yberdnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
